92b58d6ccf0e1f25d27d540a20b8147852259c00
Jorge Alejandro Quiroz Serna <alejandro.devop@gmail.com>
2023-03-10 19:07:04 -0500

    Added some operators info

--- LearningSwiftUdemy.playground/Pages/Operators.xcplaygroundpage/Contents.swift
+++ LearningSwiftUdemy.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -2,6 +2,61 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+/**
+ * Arithmetic operators
+ * `=` Asignation
+ * `+=` Incremental
+ * `+` Addition
+ * `-` Subtraction
+ * `*` Multiplication
+ * `/` Division
+ * `%` Mod
+ *
+ * Comparison operators
+ * `==` Equality
+ * `!=` Different
+ * `>` Greater than
+ * `<` Less than
+ * `>=` Greater or equals
+ * `<=` Less or equals
+ *
+ * Identity Operator
+ * `===` Identical to
+ * `!==` Not identical to
+ */
 
-//: [Next](@next)
+/**
+ * Nil-Coalescing Operator
+ * `??` This operator simplifies a short conditional (terniary)
+ */
+let a = "Foo"
+let b = "Bar"
+print(a != nil ? a : b) // This could be simplified by `??`
+print(a ?? b)
+
+/**
+ * Range operator
+ * `0...9`
+ * `0..<10`
+ * `...9`
+ */
+
+/**
+ * Logical Operators
+ * `&&` And
+ * `||` Or
+ * `!` Negation
+ * True table
+ * |  bool  |  bool  |  Operator  | Result |
+ * |:------:|:------:|:----------:|:------:|
+ * | `true`   | `true`    |    `&&`    | `true`    |
+ * | `true`   | `false`  |    `&&`    | `false` |
+ * | `false` | `false`  |    `&&`    | `false` |
+ * | `true`   | `false`  |    `&&`    | `false` |
+ * | `true`   | `true`    |    `||`    | `true`   |
+ * | `false` | `true`    |    `||`    | `true`   |
+ * | `true`   | `false`  |    `||`    | `true`   |
+ * | `false` | `false`  |    `||`    | `false` |
+ * | `!true` |                  |                 | `false`  |
+ * |`!false`|                  |                 | `true`    |
+ */
